b7c3e13e0ced019f3fc95ca4d4f0f319dc138773
alone0x <49860410+alone0x@users.noreply.github.com>
2019-04-25 03:24:45 -0700

    nopbai

---
+++ cdtt.js
@@ -0,0 +1,9 @@
+function cdtt() {
+
+  let x= parseInt(document.getElementById("from").value) ;
+  let y=parseInt( document.getElementById("to").value);
+  let z=parseInt( document.getElementById("input").value);
+    let kq =z*y/x;
+    document.getElementById("result").value=kq;
+
+}
